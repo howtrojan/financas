7fdfb48f5b97587e9d40edbc823e7b18456668cd
Howtrojan <andre.braga.asb@gmail.com>
2024-03-18 10:25:54 -0300

    ajuste alturas

--- lib/features/onboarding/onboarding_page.dart
+++ lib/features/onboarding/onboarding_page.dart
@@ -13,66 +13,71 @@ class OnboardingPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.lavender,
-      body: SizedBox(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Image.asset(
-              'assets/images/main.png',
-            ),
-            Container(
-              alignment: Alignment.center,
-              height: MediaQuery.of(context).size.height * 0.4,
-              color: AppColors.white,
-              child: Column(children: [
-                SizedBox(
-                  height: MediaQuery.of(context).size.height * 0.05,
-                ),
-                Text('Economize mais',
-                    textAlign: TextAlign.center,
-                    style: AppTextStyles.mediumText
-                        .copyWith(color: AppColors.secundary)),
-                Text('Gaste com inteligência',
-                    textAlign: TextAlign.center,
-                    style: AppTextStyles.mediumText
-                        .copyWith(color: AppColors.primary)),
-                Padding(
-                  padding: EdgeInsets.symmetric(
-                    horizontal: MediaQuery.of(context).size.height * 0.032,
-                    vertical: MediaQuery.of(context).size.height * 0.016,
-                  ),
-                  child: PrimaryButton(
-                    text: 'Iniciar',
-                    onPressed: () {
-                      Navigator.pushNamed(context, NamedRoute.signUp);
-                    },
-                  ),
-                ),
-                CustomTextButton(
-                  onPressed: () => {},
+      body: ListView(
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Image.asset(
+                'assets/images/main.png',
+              ),
+              Container(
+                alignment: Alignment.center,
+                height: MediaQuery.of(context).size.height * 0.4,
+                color: AppColors.white,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
+                    SizedBox(height: MediaQuery.of(context).size.height * 0.05),
                     Text(
-                      'Não tem conta?',
+                      'Economize mais',
                       textAlign: TextAlign.center,
-                      style: AppTextStyles.smallText.copyWith(
-                        color: AppColors.darkGrey,
-                      ),
+                      style: AppTextStyles.mediumText
+                          .copyWith(color: AppColors.secundary),
                     ),
                     Text(
-                      'Cadastre-se',
+                      'Gaste com inteligência',
                       textAlign: TextAlign.center,
-                      style: AppTextStyles.smallText.copyWith(
-                        color: AppColors.primary,
+                      style: AppTextStyles.mediumText
+                          .copyWith(color: AppColors.primary),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: MediaQuery.of(context).size.height * 0.032,
+                        vertical: MediaQuery.of(context).size.height * 0.016,
                       ),
+                      child: PrimaryButton(
+                        text: 'Iniciar',
+                        onPressed: () {
+                          Navigator.pushNamed(context, NamedRoute.signUp);
+                        },
+                      ),
+                    ),
+                    CustomTextButton(
+                      onPressed: () {},
+                      children: [
+                        Text(
+                          'Não tem conta?',
+                          textAlign: TextAlign.center,
+                          style: AppTextStyles.smallText.copyWith(
+                            color: AppColors.darkGrey,
+                          ),
+                        ),
+                        Text(
+                          'Cadastre-se',
+                          textAlign: TextAlign.center,
+                          style: AppTextStyles.smallText.copyWith(
+                            color: AppColors.primary,
+                          ),
+                        ),
+                      ],
                     ),
                   ],
                 ),
-              ]),
-            ),
-          ],
-        ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
